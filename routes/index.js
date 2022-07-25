f9604997b089dcbf3e062577ac1a607ddcf6aaa5
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-24 22:59:40 -0500

    Removed commented out code in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,6 @@ import { router as apiRoutes } from "./api/index.js";
 
 const router = expressRouter();
 
-// router.get('/', (req, res) => {
-//     res.send("Hello World!");
-// })
-
-// router.post('/signup', (req, res) => {
-//     const { userName, password, email } = req.body;
-//     const user = new User({ userName, password, email });
-//     user.save()
-//         .then(() => res.send({ message: "User created!", data: user }))
-//         .catch(err => res.send(err));
-
-// })
-
-// router.route('/users')
-//     .get(getAllUsers)
-
-
 router.use('/api', apiRoutes);
 
 router.use((_, res) => res.status(404).json('Wrong route!'));
