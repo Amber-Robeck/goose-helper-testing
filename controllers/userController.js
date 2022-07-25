8e938b458fa295f49b96f12609b79f71a81487bb
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-24 23:02:25 -0500

    Added and tested createNewUser in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,10 @@ import gooseHelper from "goose-helper";
 
 function getAllUsers(req, res) {
     gooseHelper(req, res, User.find({}));
+};
+
+function createNewUser(req, res) {
+    gooseHelper(req, res, User.create(req.body));
 }
 
-export { getAllUsers };
+export { getAllUsers, createNewUser };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,12 @@
 import { Router as expressRouter } from "express";
-import { getAllUsers } from "../../controllers/userController.js";
+import { getAllUsers, createNewUser } from "../../controllers/userController.js";
 const router = expressRouter();
 
 //api/users endpoint
 
 router.route('/')
     .get(getAllUsers)
+    .post(createNewUser)
 
 
 export { router };
